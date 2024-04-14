ab2ded19c22e32ffa0f27585cb8c0612d245c7d3
kiarahmani <kia@durable.ai>
2024-04-14 16:43:02 -0700

    [SYNTHESIS] synthesizer is implemented and basic functionalities are tested.

--- dsl_examples.py
+++ dsl_examples.py
@@ -1,23 +1,62 @@
-from dsl import *
+from dsl import WhereClause, Count, Query, Map
 
 # Count the number of cups
-ex_1 = Count(Query('cups', WhereClause('true', 'cups')), 'cups')
+program = Count(
+    query=Query(
+        obj_tp="cups", where_clause=WhereClause(where_tp="true", obj_tp="cups")
+    ),
+    obj_tp="cups",
+)
 
-print(ex_1.pretty_str())
+print(program.pretty_str())
 
 
 # Find vacant conference room
-sub_where1 = WhereClause('not', 'Conference Room', sub_where_clause=[WhereClause('spatial_rel', 'Conference Room', obj_tp2='Human', spatial_relation='inside')])
-sub_where2 = WhereClause('spatial_rel', 'Conference Room', obj_tp2='White Board', spatial_relation='inside')
-ex_2 = Map('Conference Room', 'location', Query('Conference Room', WhereClause('and', 'Conference Room', sub_where_clause=[sub_where1, sub_where2])))
+sub_where1 = WhereClause(
+    where_tp="not",
+    obj_tp="Conference Room",
+    sub_where_clause=[
+        WhereClause(
+            where_tp="spatial_rel",
+            obj_tp="Conference Room",
+            obj_tp2="Human",
+            spatial_relation="inside",
+        )
+    ],
+)
+sub_where2 = WhereClause(
+    where_tp="spatial_rel",
+    obj_tp="Conference Room",
+    obj_tp2="White Board",
+    spatial_relation="inside",
+)
+program = Map(
+    obj_tp="Conference Room",
+    map_feature="location",
+    query=Query(
+        obj_tp="Conference Room",
+        where_clause=WhereClause(
+            where_tp="and",
+            obj_tp="Conference Room",
+            sub_where_clause=[sub_where1, sub_where2],
+        ),
+    ),
+)
 
 
 print(sub_where1.pretty_str())
 print(sub_where2.pretty_str())
 
-print(ex_2.pretty_str())
+print(program.pretty_str())
 
 
 # Where is my tallest cup
-ex_3 = Map('Cup', 'location', Query('Cup', WhereClause('max', 'cup', scalar_feature='height')))
-print(ex_3.pretty_str())
+program = Map(
+    obj_tp="Cup",
+    map_feature="location",
+    query=Query(
+        obj_tp="Cup",
+        where_clause=WhereClause(where_tp="max", obj_tp="cup", scalar_feature="height"),
+    ),
+)
+print(program.pretty_str())
